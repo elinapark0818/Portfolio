9e6324cd057da36837ab72430a097daf31b36c49
ElinaPark <elinapark0818@gmail.com>
2021-04-15 15:57:52 +0900

    JS transparent

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight: ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     }else {
@@ -44,10 +44,23 @@ const contactMe = document.querySelector('.home__contact');
 contactMe.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 });
-    function scrollIntoView(selector) {
-        const scrollTo = document.querySelector(selector);
-        scrollTo.scrollIntoView({behavior: "smooth"});
-    }
+
+
+//Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
 
 
+
+
+
+
+// Utility function
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
