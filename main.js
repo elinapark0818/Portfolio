010ac8c5f87a0fa19ca6ad707cf0b57b2c92679d
ElinaPark <elinapark0818@gmail.com>
2021-04-20 22:45:39 +0900

    JS Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight)

--- main.js
+++ main.js
@@ -177,7 +177,10 @@ sections.forEach(section => observer.observe(section));
 window.addEventListener('wheel', () => {
     if (window.scrollY === 0) {
         selectedNavIndex = 0;
-    } else if (window.scrollY + window.innerHeight === document.body.clientHeight) {
+
+    } else if (
+        Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight)
+    {
         selectedNavIndex = navItems.length - 1;
     }
     selectNavItem(navItems[selectedNavIndex]);
